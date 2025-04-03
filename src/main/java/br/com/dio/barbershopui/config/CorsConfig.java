d16373d7c78ee8c7dd1097ef9b81f996875f0915
Geovanna Thereza <118942364+geovannathereza@users.noreply.github.com>
2025-04-03 16:57:08 -0300

    Update CorsConfig.java

--- src/main/java/br/com/dio/barbershopui/config/CorsConfig.java
+++ src/main/java/br/com/dio/barbershopui/config/CorsConfig.java
@@ -6,14 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
 import java.util.Collections;
-
 import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
         var config = new CorsConfiguration();
@@ -28,8 +25,6 @@ public class CorsConfig {
         FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>();
         bean.setFilter(new CorsFilter(source));
         bean.setOrder(HIGHEST_PRECEDENCE);
-
         return bean;
     }
-
 }
