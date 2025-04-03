bff71a7bf13c6fe5dbc50ea8bef3f4ab647fea20
Geovanna Thereza <118942364+geovannathereza@users.noreply.github.com>
2025-04-03 16:51:01 -0300

    Update EmailInUseException.java

--- src/main/java/br/com/dio/barbershopui/exception/EmailInUseException.java
+++ src/main/java/br/com/dio/barbershopui/exception/EmailInUseException.java
@@ -1,9 +1,5 @@
 package br.com.dio.barbershopui.exception;
 
 public class EmailInUseException extends RuntimeException {
-
-    public EmailInUseException(String message) {
-        super(message);
-    }
-
+    public EmailInUseException(String message) { super(message); }
 }
