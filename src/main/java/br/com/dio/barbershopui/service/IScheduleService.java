9f3ecef9d3985efead6dcbe5e9b497fccd326987
Geovanna Thereza <118942364+geovannathereza@users.noreply.github.com>
2025-04-03 16:43:44 -0300

    Update IScheduleService.java

--- src/main/java/br/com/dio/barbershopui/service/IScheduleService.java
+++ src/main/java/br/com/dio/barbershopui/service/IScheduleService.java
@@ -3,9 +3,6 @@ package br.com.dio.barbershopui.service;
 import br.com.dio.barbershopui.entity.ScheduleEntity;
 
 public interface IScheduleService {
-
     ScheduleEntity save(final ScheduleEntity entity);
-
     void delete(final long id);
-
 }
