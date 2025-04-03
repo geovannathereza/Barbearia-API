28032b2bf5dcd23a4e033ba61897b3f2c4844624
Geovanna Thereza <118942364+geovannathereza@users.noreply.github.com>
2025-04-03 16:50:42 -0300

    Update NotFoundException.java

--- src/main/java/br/com/dio/barbershopui/exception/NotFoundException.java
+++ src/main/java/br/com/dio/barbershopui/exception/NotFoundException.java
@@ -1,9 +1,5 @@
 package br.com.dio.barbershopui.exception;
 
 public class NotFoundException extends RuntimeException {
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-
+    public NotFoundException(String message) { super(message); }
 }
