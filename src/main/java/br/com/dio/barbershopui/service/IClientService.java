96ff7ab7ba24e3760ade27156a01a42a96e0d2cd
Geovanna Thereza <118942364+geovannathereza@users.noreply.github.com>
2025-04-03 16:44:02 -0300

    Update IClientService.java

--- src/main/java/br/com/dio/barbershopui/service/IClientService.java
+++ src/main/java/br/com/dio/barbershopui/service/IClientService.java
@@ -3,11 +3,7 @@ package br.com.dio.barbershopui.service;
 import br.com.dio.barbershopui.entity.ClientEntity;
 
 public interface IClientService {
-
     ClientEntity save(final ClientEntity entity);
-
     ClientEntity update(final ClientEntity entity);
-
     void delete(final long id);
-
 }
