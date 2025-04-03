4fcd3139a3b5109c60eb096a255eed375b5927f8
Geovanna Thereza <118942364+geovannathereza@users.noreply.github.com>
2025-04-03 16:46:36 -0300

    Update ScheduleService.java

--- src/main/java/br/com/dio/barbershopui/service/impl/ScheduleService.java
+++ src/main/java/br/com/dio/barbershopui/service/impl/ScheduleService.java
@@ -10,14 +10,12 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class ScheduleService implements IScheduleService {
-
     private final IScheduleRepository repository;
     private final IScheduleQueryService queryService;
 
     @Override
     public ScheduleEntity save(final ScheduleEntity entity) {
         queryService.verifyIfScheduleExists(entity.getStartAt(), entity.getEndAt());
-
         return repository.save(entity);
     }
 
